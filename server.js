940ee2e8dc6a2956e0920f4add32ae53274121aa
dzsabba <csabiabig@gmail.com>
2023-11-21 08:20:49 +0100

    :sparkles: added mongoose

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const trainings = require('./routes/trainings')
-
 require('dotenv').config() // A .env fájlt olvassanp
+
+
 const app = express()
+
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+const mongoString = process.env.DATABASE_URL;
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+database.on("error", (error) => {
+    console.log(error);
+});
+database.once("connected", () => {
+    console.log(`Database Connected ${database.host}`);
+});
+
 const morgan = require('morgan')
 
 app.use(morgan('dev'))
